58b07d7b10b9ea5041d57e73a624a3f1b8479864
Dylan Piercey <dpiercey@ebay.com>
2021-06-28 11:42:13 -0700

    feat: add props tag

---
+++ src/components/props/translator.ts
@@ -0,0 +1,32 @@
+import type { types } from "@marko/compiler";
+import deepFreeze from "../../util/deep-freeze/transform";
+
+export = (tag: types.NodePath<types.MarkoTag>, t: typeof types) => {
+  const tagVar = tag.node.var!;
+  const errorMessage = !tagVar
+    ? "requires a tag variable to be assigned to"
+    : tag.node.attributes.length > 0
+    ? "does not support attributes"
+    : tag.node.body.body.length
+    ? "does not support body content"
+    : tag.node.body.params.length
+    ? "does not support tag body parameters"
+    : tag.node.arguments?.length
+    ? "does not support arguments"
+    : undefined;
+
+  if (errorMessage) {
+    throw tag
+      .get("name")
+      .buildCodeFrameError(`The <props> tag ${errorMessage}.`);
+  }
+
+  tag.replaceWith(
+    t.variableDeclaration("const", [
+      t.variableDeclarator(
+        tagVar,
+        deepFreeze(tag.hub.file, t.identifier("input"))
+      ),
+    ])
+  );
+};
--- src/util/deep-freeze/index.ts
+++ src/util/deep-freeze/index.ts
@@ -1,5 +1,5 @@
 export = function deepFreeze(val: unknown) {
-  if (!Object.isFrozen(val)) {
+  if (!Object.isFrozen(val) && typeof val !== "function") {
     Object.freeze(val);
     for (const key in val as Record<string, unknown>) {
       deepFreeze((val as Record<string, unknown>)[key]);
