9be91f462a242b2da940c6e882cac8e8aef390d3
Dylan Piercey <dpiercey@ebay.com>
2021-07-02 08:05:16 -0700

    fix: prevent multiple props tags

--- src/components/props/translator.ts
+++ src/components/props/translator.ts
@@ -1,9 +1,13 @@
 import type { types } from "@marko/compiler";
+import { Hub } from "@marko/compiler/babel-types";
 import deepFreeze from "../../util/deep-freeze/transform";
+const usedTag = new WeakSet<Hub>();
 
 export = (tag: types.NodePath<types.MarkoTag>, t: typeof types) => {
   const tagVar = tag.node.var!;
-  const errorMessage = !tagVar
+  const errorMessage = usedTag.has(tag.hub)
+    ? "can only be used once within a template"
+    : !tagVar
     ? "requires a tag variable to be assigned to"
     : !tag.parentPath.parentPath.isProgram()
     ? "can only used at the root of the template"
@@ -22,6 +26,8 @@ export = (tag: types.NodePath<types.MarkoTag>, t: typeof types) => {
       .get("name")
       .buildCodeFrameError(`The <props> tag ${errorMessage}.`);
   }
+
+  usedTag.add(tag.hub);
 
   tag.replaceWith(
     t.variableDeclaration("const", [
