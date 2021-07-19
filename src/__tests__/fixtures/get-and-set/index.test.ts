be0cb7392344ef5161b9c80cae27be57b67d8b22
Dylan Piercey <dpiercey@ebay.com>
2021-07-19 12:30:04 -0700

    chore: add tests for get tag error states

--- src/__tests__/fixtures/get-and-set/index.test.ts
+++ src/__tests__/fixtures/get-and-set/index.test.ts
@@ -19,3 +19,35 @@ describe(
   "<get> & <set> provide through multiple children",
   fixture("./templates/provide-across-children/index.marko")
 );
+
+describe("error <get> args", fixture("./templates/error-get-args.marko"));
+
+describe(
+  "error <get> body content",
+  fixture("./templates/error-get-body-content.marko")
+);
+
+describe(
+  "error <get> body parameters",
+  fixture("./templates/error-get-body-parameters.marko")
+);
+
+describe(
+  "error <get> extra attr",
+  fixture("./templates/error-get-extra-attr.marko")
+);
+
+describe(
+  "error <get> missing tag",
+  fixture("./templates/error-get-missing-tag.marko")
+);
+
+describe(
+  "error <get> no default attr",
+  fixture("./templates/error-get-no-default-attr.marko")
+);
+
+describe(
+  "error <get> no tag var",
+  fixture("./templates/error-get-no-tag-var.marko")
+);
