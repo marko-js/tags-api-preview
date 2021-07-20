1c712906fe7f8704a8a2fda19a3b592c4f4d3b8e
Dylan Piercey <dpiercey@ebay.com>
2021-07-20 09:08:34 -0700

    chore: add tests for tag tag error states

--- src/__tests__/fixtures/tag/index.test.ts
+++ src/__tests__/fixtures/tag/index.test.ts
@@ -1,3 +1,22 @@
 import fixture from "../../fixture";
 
 describe("<tag> basic", fixture("./templates/basic.marko"));
+
+describe("<tag> error args", fixture("./templates/error-args.marko"));
+
+describe("<tag> error attrs", fixture("./templates/error-attrs.marko"));
+
+describe(
+  "<tag> error destructured",
+  fixture("./templates/error-destructured.marko")
+);
+
+describe(
+  "<tag> error no content",
+  fixture("./templates/error-no-content.marko")
+);
+
+describe(
+  "<tag> error no tag var",
+  fixture("./templates/error-no-tag-var.marko")
+);
