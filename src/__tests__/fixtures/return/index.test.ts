208c87add74642c86e16c2d5c88dba89eb2c7cba
Dylan Piercey <dpiercey@ebay.com>
2021-07-20 08:49:14 -0700

    chore: add tests for return tag error states

--- src/__tests__/fixtures/return/index.test.ts
+++ src/__tests__/fixtures/return/index.test.ts
@@ -7,6 +7,32 @@ describe(
   fixture("./templates/write-from-parent.marko", [click("increment")])
 );
 
+describe("<return> error args", fixture("./templates/error-args.marko"));
+
+describe(
+  "<return> error body content",
+  fixture("./templates/error-body-content.marko")
+);
+
+describe(
+  "<return> error body parameters",
+  fixture("./templates/error-body-parameters.marko")
+);
+
+describe(
+  "<return> error multiple",
+  fixture("./templates/error-multiple.marko")
+);
+
+describe("<return> error nested", fixture("./templates/error-nested.marko"));
+
+describe(
+  "<return> error no default attr",
+  fixture("./templates/error-no-default-attr.marko")
+);
+
+describe("<return> error tag var", fixture("./templates/error-tag-var.marko"));
+
 function click(text: string) {
   return async ({ fireEvent, screen }: FixtureHelpers) =>
     await fireEvent.click(screen.getByText(text));
