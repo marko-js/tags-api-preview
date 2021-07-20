2c68a3d711b18e301dc0317758a8ff7dea73c487
Dylan Piercey <dpiercey@ebay.com>
2021-07-19 19:56:39 -0700

    chore: add default change test for let tag

--- src/__tests__/fixtures/let/index.test.ts
+++ src/__tests__/fixtures/let/index.test.ts
@@ -1,32 +1,47 @@
+import { spy, resetHistory } from "sinon";
 import fixture, { FixtureHelpers } from "../../fixture";
+
+const valueChange = spy();
+const increment = click("Increment");
 
 describe(
   "<let> basic",
-  fixture("./templates/basic.marko", [
+  fixture("./templates/basic.marko", [{ value: 1 }, increment, increment])
+);
+
+describe(
+  "<let> with default change",
+  fixture("./templates/with-default-change.marko", [
+    { value: 1, valueChange },
+    async (helpers) => {
+      await increment(helpers);
+      helpers.expect(valueChange).to.have.been.calledOnceWith(2);
+      resetHistory();
+    },
+    { value: 2, valueChange },
     { value: 1 },
-    click("Increment"),
-    click("Increment"),
+    async (helpers) => {
+      await increment(helpers);
+      helpers.expect(valueChange).has.not.been.called;
+      resetHistory();
+    },
   ])
 );
 
 describe(
   "<let> hoisted",
-  fixture("./templates/hoisted.marko", [
-    { value: 1 },
-    click("Increment"),
-    click("Increment"),
-  ])
+  fixture("./templates/hoisted.marko", [{ value: 1 }, increment, increment])
 );
 
 describe(
   "<let> within-condition",
   fixture("./templates/within-condition.marko", [
     { show: true, value: 0 },
-    click("Increment"),
-    click("Increment"),
+    increment,
+    increment,
     { show: false },
     { show: true, value: 1 },
-    click("Increment"),
+    increment,
   ])
 );
 
