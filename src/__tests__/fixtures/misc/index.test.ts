753e0fac5ea1b6b3bd2c152f1a90a8cc1be87c6d
Dylan Piercey <dpiercey@ebay.com>
2021-07-21 09:40:50 -0700

    chore: add feature detection tests

--- src/__tests__/fixtures/misc/index.test.ts
+++ src/__tests__/fixtures/misc/index.test.ts
@@ -2,54 +2,120 @@ import fixture, { FixtureHelpers } from "../../fixture";
 
 const increment = click("increment");
 
-describe(
-  "misc assign with default",
-  fixture("./templates/assign-with-default.marko", [increment, increment])
-);
+describe("misc assign", () => {
+  describe(
+    "with default",
+    fixture("./templates/assign-with-default.marko", [increment, increment])
+  );
 
-describe(
-  "misc assign with computed",
-  fixture("./templates/assign-with-computed.marko", [increment, increment])
-);
+  describe(
+    "with computed",
+    fixture("./templates/assign-with-computed.marko", [increment, increment])
+  );
 
-describe(
-  "misc assign with default and rest",
-  fixture("./templates/assign-with-default-and-rest.marko", [
-    increment,
-    increment,
-  ])
-);
+  describe(
+    "with default and rest",
+    fixture("./templates/assign-with-default-and-rest.marko", [
+      increment,
+      increment,
+    ])
+  );
 
-describe(
-  "misc assign with string-literal",
-  fixture("./templates/assign-with-string-literal.marko", [
-    increment,
-    increment,
-  ])
-);
+  describe(
+    "with string-literal",
+    fixture("./templates/assign-with-string-literal.marko", [
+      increment,
+      increment,
+    ])
+  );
 
-describe(
-  "misc assign with existing change handler",
-  fixture("./templates/assign-with-existing-change-handler.marko", [
-    increment,
-    increment,
-  ])
-);
+  describe(
+    "with existing change handler",
+    fixture("./templates/assign-with-existing-change-handler.marko", [
+      increment,
+      increment,
+    ])
+  );
 
-describe(
-  "misc error assign-to-rest-element",
-  fixture("./templates/error-assign-to-rest-element.marko")
-);
+  describe(
+    "error assign-to-rest-element",
+    fixture("./templates/error-assign-to-rest-element.marko")
+  );
 
-describe(
-  "misc error assign-to-array-item",
-  fixture("./templates/error-assign-to-array-item.marko")
-);
+  describe(
+    "error assign-to-array-item",
+    fixture("./templates/error-assign-to-array-item.marko")
+  );
 
-describe(
-  "misc error assign-to-tag-param",
-  fixture("./templates/error-assign-to-tag-param.marko")
-);
+  describe(
+    "error assign-to-tag-param",
+    fixture("./templates/error-assign-to-tag-param.marko")
+  );
+});
+
+describe("misc feature detect", () => {
+  describe(
+    "class by default",
+    fixture("./templates/feature-detect-class-by-default.marko")
+  );
+
+  describe(
+    "tags comment directive",
+    fixture("./templates/feature-detect-tags-comment-directive.marko")
+  );
+
+  describe("tags var", fixture("./templates/feature-detect-tags-var.marko"));
+
+  describe(
+    "tags new tag",
+    fixture("./templates/feature-detect-tags-new-tag.marko")
+  );
+
+  describe(
+    "error arg events",
+    fixture("./templates/error-feature-detect-arg-events.marko")
+  );
+
+  describe(
+    "error attr modifier",
+    fixture("./templates/error-feature-detect-attr-modifier.marko")
+  );
+
+  describe(
+    "error class block",
+    fixture("./templates/error-feature-detect-class-block.marko")
+  );
+
+  describe(
+    "error legacy attrs",
+    fixture("./templates/error-feature-detect-legacy-attrs.marko")
+  );
+
+  describe(
+    "error legacy tag",
+    fixture("./templates/error-feature-detect-legacy-tag.marko")
+  );
+
+  describe(
+    "error reference component",
+    fixture("./templates/error-feature-detect-reference-component.marko")
+  );
+
+  describe(
+    "error reference input",
+    fixture("./templates/error-feature-detect-reference-input.marko")
+  );
+
+  describe(
+    "error reference out",
+    fixture("./templates/error-feature-detect-reference-out.marko")
+  );
+
+  describe(
+    "error scriptlet",
+    fixture("./templates/error-feature-detect-scriptlet.marko")
+  );
+});
 
 function click(text: string) {
   return async ({ fireEvent, screen }: FixtureHelpers) =>
