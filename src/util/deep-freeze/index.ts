293050d8d643729bb6d35ea815688f199bfd5fb9
Dylan Piercey <dpiercey@ebay.com>
2021-07-27 09:00:38 -0700

    fix: disable Object freeze in SSR mode

--- src/util/deep-freeze/index.ts
+++ src/util/deep-freeze/index.ts
@@ -1,10 +1,3 @@
 export = function deepFreeze(val: unknown) {
-  if (!Object.isFrozen(val) && typeof val !== "function") {
-    Object.freeze(val);
-    for (const key in val as Record<string, unknown>) {
-      deepFreeze((val as Record<string, unknown>)[key]);
-    }
-  }
-
   return val;
 };
