6f9840c1bf4ee75bedb29424c870626a88d3cb1e
Dylan Piercey <dpiercey@ebay.com>
2021-10-28 08:41:25 -0700

    fix: issue with multiple gets before attrs/return

--- src/util/is-at-root.ts
+++ src/util/is-at-root.ts
@@ -1,9 +1,12 @@
 import { types as t } from "@marko/compiler";
 import isCoreTag from "./is-core-tag";
 
-export default function isAtRoot(tag: t.NodePath<t.MarkoTag>) {
+export default function isAtRoot(tag: t.NodePath<t.MarkoTag>): boolean {
   const parentPath = tag.parentPath.parentPath!;
   // Special case `<get>` since it currently wraps it's children
   // which is an implementation detail.
-  return parentPath!.isProgram() || isCoreTag("get", parentPath);
+  return (
+    parentPath!.isProgram() ||
+    (isCoreTag("get", parentPath) && isAtRoot(parentPath))
+  );
 }
