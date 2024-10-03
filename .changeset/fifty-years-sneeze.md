2fba2c9df78462b7b645a483a23f25acb7a4a3df
dpiercey <dpiercey@ebay.com>
2024-10-03 15:24:46 -0700

    fix: tell @marko/compiler about function event handlers

---
+++ .changeset/fifty-years-sneeze.md
@@ -0,0 +1,5 @@
+---
+"@marko/tags-api-preview": patch
+---
+
+Use `meta.hasFunctionEventHandlers` from recent marko releases when we detect an event or change handler to ensure the template is marked for client side compilation.`
--- src/transform/index.ts
+++ src/transform/index.ts
@@ -2,12 +2,14 @@ import type { types as t } from "@marko/compiler";
 import wrapperComponent from "./wrapper-component";
 import cachedValues from "./cached-values/transform";
 import nativeTagVar from "./native-tag-var/transform";
+import nativeTagHandlers from "./track-function-handlers";
 import hoistTagVars from "./hoist-tag-vars/transform";
 import featureDetection from "./feature-detection";
 import tagBodyParameters from "./tag-body-parameters";
 import customTagVar from "./custom-tag-var";
 import assignmentsToChangeCall from "./assignments-to-change-call";
 import attributeBindings from "./attribute-bindings";
+import trackFunctionHandlers from "./track-function-handlers";
 
 export default [
   featureDetection,
@@ -17,6 +19,8 @@ export default [
   hoistTagVars,
   attributeBindings,
   nativeTagVar,
+  nativeTagHandlers,
   customTagVar,
   tagBodyParameters,
+  trackFunctionHandlers,
 ] as t.Visitor[];
